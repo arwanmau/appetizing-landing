df2de2b28b5882ad12352880fa300ae647d39f7f
arwanmau <arwanmaulana028@gmail.com>
2026-05-02 15:39:21 +0700

    setup project

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
-=======
 // @lovable.dev/vite-tanstack-config already includes the following — do NOT add them manually
 // or the app will break with duplicate plugins:
 //   - tanstackStart, viteReact, tailwindcss, tsConfigPaths, cloudflare (build-only),
@@ -30,4 +7,3 @@ export default defineConfig(({ mode }) => ({
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig();
->>>>>>> 19c33f3c2723ce9066763c84e3ed774b6b7e4d95
